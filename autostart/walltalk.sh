d5f5633d10d724cc4b673f807bb745a78fff943a
scheuerell <mikejscheuerell@gmail.com>
2018-02-05 00:34:48 +0000

    adding autostart files

---
+++ autostart/walltalk.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#exit
+
+#set timezone
+TZ='America/Menominee'; export TZ
+
+# start jack server
+#/usr/bin/jackd -dalsa -dhw:C320,0 -r48000 -p1024 -n2 &
+/usr/bin/jackd -dalsa -dhw:Device -r48000 -p1024 -n2 &
+# wait for jack server to be available
+jack_wait -w
+# start walltalk
+walltalk -c 1 -s -f wav &
+# connect mic port to walltalk
+sleep 5
+/usr/bin/jack_connect system:capture_1 walltalk:in_1
+while [ ! $? -eq 0 ]
+do
+	/usr/bin/jack_connect system:capture_1 walltalk:in_1
+	sleep 2
+done
