3f22557bbbabb500306b3252f7a6e44cd77e2c9e
scheuerell <mikejscheuerell@gmail.com>
2018-03-30 20:59:00 -0500

    added max volume control

--- autostart/walltalk.sh
+++ autostart/walltalk.sh
@@ -3,6 +3,9 @@
 #set gpio 2 (wiringpi gpio ID) to high-impedance (no pullup/down)
 #gpio mode 2 tri
 
+# set mic volume to max (range is 0-16)
+amixer -D hw:Device cset numid=3 16
+
 # start jack server
 #/usr/bin/jackd -dalsa -dhw:C320,0 -r48000 -p1024 -n2 &
 /usr/bin/jackd -dalsa -dhw:Device -r48000 -p1024 -n2 &
