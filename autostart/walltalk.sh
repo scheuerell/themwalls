55fd64c6d6e19653c7aa5aec434e22f3561af47f
scheuerell <mikejscheuerell@gmail.com>
2018-05-08 21:32:00 -0500

    adding access point setup files

--- autostart/walltalk.sh
+++ autostart/walltalk.sh
@@ -24,3 +24,13 @@ done
 
 #set timezone
 printf "TZ='America/Menominee'; export TZ" >> /home/pi/.profile
+
+#add wifi interface for access point
+sudo /sbin/iw phy phy0 interface add ap0 type __ap
+sudo /bin/ip link set ap0 address b8:27:eb:79:18:b0
+
+sleep 30
+#sudo ifdown --force wlan0 && sudo ifdown --force ap0 && sudo ifup ap0 && sudo ifup wlan0
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -s 192.168.10.0/24 ! -d 192.168.10.0/24 -j MASQUERADE
+sudo systemctl restart dnsmasq 
